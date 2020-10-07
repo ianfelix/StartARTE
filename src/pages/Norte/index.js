5a04ba2a0bad8383cf7894eefb51e72967631000
Michelle Soto <michelle.varas.soto@outlook.com>
2020-10-06 23:55:28 -0300

    imagens norte

--- src/pages/Norte/index.js
+++ src/pages/Norte/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ImgCard from '../../components/Card/ImgCard';
 import imageNorte from '../../assets/images/cangaceiros.jpg';
+import artesã from '../../assets/images/artesã.jpg';
+import arteIndigena from '../../assets/images/indigena.png';
+import mestre from '../../assets/images/mestre.png';
 import './styles.css';
 
 const Norte = () => {
   return (
     <div className='margin-body animeDown'>
       <div className='container-flex-norte'>
-        <ImgCard img={imageNorte} />
-        <ImgCard img={imageNorte} />
-        <ImgCard img={imageNorte} />
+        <ImgCard img={artesã} />
+        <ImgCard img={arteIndigena} />
+        <ImgCard img={mestre} />
       </div>
     </div>
   );
