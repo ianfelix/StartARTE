33edf3bc942d10143e6c9a4eb9bf96a75a13180a
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2020-10-07 01:24:33 -0300

    Added images and texts

--- src/pages/Norte/index.js
+++ src/pages/Norte/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ImgCard from '../../components/Card/ImgCard';
-import imageNorte from '../../assets/images/cangaceiros.jpg';
-import artesã from '../../assets/images/artesã.jpg';
+import decoracao from '../../assets/images/decoracao.jpg';
 import arteIndigena from '../../assets/images/indigena.png';
-import mestre from '../../assets/images/mestre.png';
+import mestre from '../../assets/images/mestre.jpg';
 import './styles.css';
+import CardBody from '../../components/Card/CardBody';
 
 const Norte = () => {
   return (
     <div className='margin-body animeDown'>
       <div className='container-flex-norte'>
-        <ImgCard img={artesã} />
-        <ImgCard img={arteIndigena} />
-        <ImgCard img={mestre} />
+        <div>
+          <ImgCard img={arteIndigena} />
+          <CardBody titleCard='Arte indígena' />
+        </div>
+        <div>
+          <ImgCard img={mestre} />
+          <CardBody titleCard='Mestres' />
+        </div>
+        <div>
+          <ImgCard img={decoracao} />
+          <CardBody titleCard='Decoração' />
+        </div>
       </div>
     </div>
   );
