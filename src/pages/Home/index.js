96081b39420e6a29e2cce6a928984d54a8e9ddf3
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2020-10-06 23:21:36 -0300

    Merge branch 'feature/card' into develop

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Card from '../../components/Card';
 import Head from '../../components/Head';
 import Sobre from '../../components/Sobre';
+import ImgCard from '../../components/Card/ImgCard';
+import imgNorte from '../../assets/images/img-norte-removebg-preview.png';
+import ImgNordeste from '../../assets/images/img-nordeste-removebg-preview.png';
+import CardBody from '../../components/Card/CardBody';
+import { NavLink } from 'react-router-dom';
+import './styles.css';
 
 import './styles.css';
 
@@ -10,7 +16,26 @@ const Home = () => {
     <div className='margin-body animeDown'>
       <Head title='Home' description='Essa é a página Home' />
       <Sobre />
-      <Card />
+      <Card>
+        <div className='card-content'>
+          <NavLink to='/norte'>
+            <ImgCard img={imgNorte} title='Imagem do norte' />
+          </NavLink>
+          <CardBody
+            titleCard='Cultura Norte'
+            title=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quis repellendus neque animi maiores optio voluptatibus laboriosam at adipisci suscipit architecto ea illum facere fugit ratione veritatis dolorum, molestias totam.'
+          />
+        </div>
+        <div className='card-content'>
+          <NavLink to='/nordeste'>
+            <ImgCard img={ImgNordeste} title='Imagem do nordeste' />
+          </NavLink>
+          <CardBody
+            titleCard='Cultura Nordeste'
+            title=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quis repellendus neque animi maiores optio voluptatibus laboriosam at adipisci suscipit architecto ea illum facere fugit ratione veritatis dolorum, molestias totam.'
+          />
+        </div>
+      </Card>
     </div>
   );
 };
--- src/pages/Norte/index.js
+++ src/pages/Norte/index.js
@@ -3,15 +3,35 @@ import ImgCard from '../../components/Card/ImgCard';
 import imageNorte from '../../assets/images/cangaceiros.jpg';
 import './styles.css';
 import Head from '../../components/Head';
+import CardBody from '../../components/Card/CardBody';
+import { NavLink } from 'react-router-dom';
+import Card from '../../components/Card';
 
 const Norte = () => {
   return (
     <div className='margin-body animeDown'>
       <div className='container-flex-norte'>
         <Head title='Norte' description='Essa é a página da região Norte' />
-        <ImgCard img={imageNorte} />
-        <ImgCard img={imageNorte} />
-        <ImgCard img={imageNorte} />
+        <Card>
+          <div className='card-content'>
+            <NavLink to='/norte'>
+              <ImgCard img={imageNorte} title='Imagem do norte' />
+            </NavLink>
+            <CardBody
+              titleCard='Cultura Norte'
+              title=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quis repellendus neque animi maiores optio voluptatibus laboriosam at adipisci suscipit architecto ea illum facere fugit ratione veritatis dolorum, molestias totam.'
+            />
+          </div>
+          <div className='card-content'>
+            <NavLink to='/nordeste'>
+              <ImgCard img={imageNorte} title='Imagem do nordeste' />
+            </NavLink>
+            <CardBody
+              titleCard='Cultura Nordeste'
+              title=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quis repellendus neque animi maiores optio voluptatibus laboriosam at adipisci suscipit architecto ea illum facere fugit ratione veritatis dolorum, molestias totam.'
+            />
+          </div>
+        </Card>
       </div>
     </div>
   );
