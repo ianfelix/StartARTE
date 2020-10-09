3e994672c7341ca489d67b779db3965543d804f9
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2020-10-09 13:26:41 -0300

    update responsiviness

--- src/components/Sobre/index.js
+++ src/components/Sobre/index.js
@@ -10,9 +10,10 @@ const Sobre = () => {
         resultado da miscigenação de diversos grupos étnicos ao longo dos
         séculos as regiões Norte e Nordeste do país, com certeza são grandes
         representantes dessa cultura, principalmente no quesito artesanatos.
-        Visando criar uma vitrine para divulgar alguns desses trabalhos criamos
-        o startARTE, um site que faz uma ponte entre o consumidor de qualquer
-        lugar do país com os produtos e mestres das regiões Norte e Nordeste.
+        Visando criar uma vitrine para divulgar todos esses trabalhos, e ainda
+        poder auxiliá -los em suas rendas, criamos o startARTE, um site que faz
+        uma ponte entre o consumidor final de qualquer lugar do país e o
+        artesão.
       </p>
     </div>
   );
