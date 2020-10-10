92ba27abeca83c471136222cf1b178b7525ba177
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2020-10-10 01:10:49 -0300

    Added scroll to top

--- src/services/Api.js
+++ src/services/Api.js
@@ -6,11 +6,10 @@ const Api = ({ state }) => {
     <div className='map-api'>
       <iframe
         width='100%'
-        height='250'
         title='map'
         frameBorder='0'
         src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyBdLA5q-3gFOjMOlKEKdrKRvAde0uhOlC8&q=${state}`}
-        allowfullscreen></iframe>
+        allowFullScreen></iframe>
     </div>
   );
 };
